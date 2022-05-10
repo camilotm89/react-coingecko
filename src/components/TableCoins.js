3b2964b5a89094a39d20f345934f4401c4364e93
camilo <camilotm89@gmail.com>
2022-05-10 02:23:32 -0500

    first commit

---
+++ src/components/TableCoins.js
@@ -0,0 +1,31 @@
+import React from "react";
+import CoinRow from './CoinRow';
+
+const titles = ['#', 'Coin', 'Price', 'Price Change', '24h Volume']
+
+const TableCoins = ({coins}) => {
+    return(
+        
+        <table className="table table-dark mt-4 table-hover">
+            <thead>
+                <tr key="">
+                    {titles.map(title => 
+                        <td>{title}</td>
+                        )}
+                </tr>
+            </thead>
+            <tbody>
+                {coins.map((coin, index) => 
+                    <CoinRow coin={coin} key={index} index={index + 1}/>
+                )}
+                
+            </tbody>
+
+        </table>
+
+
+        
+    )
+}
+
+export default TableCoins
